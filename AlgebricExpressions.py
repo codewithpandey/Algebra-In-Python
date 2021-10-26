69137f6c55b33055a9a8b3377bfbf15c9bf32d5b
codewithpandey <abhishekap2001@gmail.com>
2021-10-26 17:24:18 +0530

    Removed unnecessary comments.

--- AlgebricExpressions.py
+++ AlgebricExpressions.py
@@ -1,10 +1,3 @@
-# Let's solve algebra using python programs.
-# In an algebric expression we don not have a numeric value which can be calculated.
-# So we have to do evry step that we do in our copies.
-# For example normally by multiplication we mean to multiply two values. But as for in algbra we have to perform a concatination operation.
-# i.e. a * b = ab
-# for division we will leave as it is i.e. a/b
-
 import re
 
 
