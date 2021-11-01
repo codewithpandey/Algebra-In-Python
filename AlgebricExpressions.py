55b3fc0bed9f5c7dc197108fbf1b740fba648ed3
codewithpandey <abhishekap2001@gmail.com>
2021-11-01 21:36:44 +0530

    Added multiplication function for multiplication without any exponents to the variables.

--- AlgebricExpressions.py
+++ AlgebricExpressions.py
@@ -42,10 +42,60 @@ def add(expression):
     print(result)
 
 def mul(expression):
-    pass
+    result = expression
+    exponent = 1
+    # let's just take both operands first
+    expRegex = re.compile(r'([0-9]?[a-zA-Z])\*([0-9]?[a-zA-Z])') # selects both co-efficients and variables.
+    operands = expRegex.search(expression)
+    operand1 = operands.group(1)
+    operand2 = operands.group(2)
+    # let's check for the co-efficients if present
+    op1Coef = ""
+    op2Coef = ""
+    for x in operand1:
+        if x.isnumeric():
+            op1Coef += x
+        else:
+            pass
+    for y in operand2:
+        if y.isnumeric():
+            op2Coef += y
+        else:
+            pass
+    # print (op1Coef, "*", op2Coef)
+    if op1Coef == "":
+        op1Coef = "1"
+    if op2Coef == "":
+        op2Coef = "1"
+    resultCoef = str(int(op1Coef)*int(op2Coef))
+
+    var1 = ''.join(filter(str.isalpha, operand1))
+    var2 = ''.join(filter(str.isalpha, operand2))
+
+    if(exponent == 1):
+        exponent *= 2
+
+    if(var1 == var2):
+        result = resultCoef + var1 + superScript(str(exponent))
+    elif(var1 != var2):
+        result = resultCoef + var1 + var2
+    else:
+        pass
+
+    print(result)
+
+# function to convert to superscript
+def superScript(e):
+    normal = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-=()"
+    super_s = "ᴬᴮᶜᴰᴱᶠᴳᴴᴵᴶᴷᴸᴹᴺᴼᴾQᴿˢᵀᵁⱽᵂˣʸᶻᵃᵇᶜᵈᵉᶠᵍʰᶦʲᵏˡᵐⁿᵒᵖ۹ʳˢᵗᵘᵛʷˣʸᶻ⁰¹²³⁴⁵⁶⁷⁸⁹⁺⁻⁼⁽⁾"
+    res = e.maketrans(''.join(normal), ''.join(super_s))
+    return e.translate(res)
 
 
+# add("a+a")
+# add("2a+3b")
+# add("2a+3a")
 
-add("a+a")
-add("2a+3b")
-add("2a+3a")
+mul("a*a")
+mul("2a*3b")
+mul("2a*3a")
