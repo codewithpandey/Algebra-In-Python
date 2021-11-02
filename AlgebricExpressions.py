54e77b45ec6de68a92d14facc6b87bc8fccd3a4a
codewithpandey <abhishekap2001@gmail.com>
2021-11-02 21:48:50 +0530

    Updated multiplication function for powers of the variables

--- AlgebricExpressions.py
+++ AlgebricExpressions.py
@@ -43,42 +43,59 @@ def add(expression):
 
 def mul(expression):
     result = expression
-    exponent = 1
     # let's just take both operands first
-    expRegex = re.compile(r'([0-9]?[a-zA-Z])\*([0-9]?[a-zA-Z])') # selects both co-efficients and variables.
+    expRegex = re.compile(r'([0-9]?[a-zA-Z]\^?[0-9]?)\*([0-9]?[a-zA-Z]\^?[0-9]?)') # selects both co-efficients and variables.
     operands = expRegex.search(expression)
     operand1 = operands.group(1)
     operand2 = operands.group(2)
+    # print(operand1, operand2)
     # let's check for the co-efficients if present
     op1Coef = ""
     op2Coef = ""
     for x in operand1:
+        if x == "^":
+            break
         if x.isnumeric():
             op1Coef += x
         else:
             pass
     for y in operand2:
+        if y == "^":
+            break
         if y.isnumeric():
             op2Coef += y
         else:
             pass
     # print (op1Coef, "*", op2Coef)
+
     if op1Coef == "":
         op1Coef = "1"
     if op2Coef == "":
         op2Coef = "1"
+
     resultCoef = str(int(op1Coef)*int(op2Coef))
+
+    if "^" in operand1:
+        expRegex = re.compile(r'(\^)([0-9])') # selects power of variable 1.
+        power = expRegex.search(operand1)
+        op1Power = power.group(2)
+    else:
+        op1Power = 1
+    if "^" in operand2:
+        expRegex = re.compile(r'(\^)([0-9])') # selects power of variable 2.
+        power = expRegex.search(operand2)
+        op2Power = power.group(2)
+    else:
+        op2Power = 1
+    # print(op1Power, op2Power)
 
     var1 = ''.join(filter(str.isalpha, operand1))
     var2 = ''.join(filter(str.isalpha, operand2))
 
-    if(exponent == 1):
-        exponent *= 2
-
     if(var1 == var2):
-        result = resultCoef + var1 + superScript(str(exponent))
+        result = resultCoef + var1 + superScript(str(int(op1Power)+int(op2Power)))
     elif(var1 != var2):
-        result = resultCoef + var1 + var2
+        result = resultCoef + var1 + superScript(str(op1Power)) + var2 + superScript(str(op2Power))
     else:
         pass
 
@@ -96,6 +113,6 @@ def superScript(e):
 # add("2a+3b")
 # add("2a+3a")
 
-mul("a*a")
-mul("2a*3b")
+mul("a^3*a^4")
+mul("2a^2*3b^3")
 mul("2a*3a")
